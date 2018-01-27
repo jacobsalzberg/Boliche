21fac84bec994134e14f604bea6a5e2196ea14f5
Jacob J. Salzberg <jacob1818@gmail.com>
2018-01-27 05:08:02 -0200

    ADD PIN.CS

---
+++ Assets/Scripts/Pin.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour {
+
+    public float strandingTreshold = 3f; // remember: anything done in the inspector OVERWRITES
+
+
+   
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    public bool IsStanding() {
+        Vector3 rotationInEuler = transform.rotation.eulerAngles;
+
+        float tiltInX = Mathf.Abs(rotationInEuler.x);
+        float tiltInY = Mathf.Abs(rotationInEuler.z);
+
+        if (tiltInX <strandingTreshold && tiltInY<strandingTreshold)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
