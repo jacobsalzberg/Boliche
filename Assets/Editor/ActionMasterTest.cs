7f7cc94841362e81a04d9ccac02164e4fa6c5281
Jacob J. Salzberg <jacob1818@gmail.com>
2018-01-29 13:43:24 -0200

    ADD TDD full routine

---
+++ Assets/Editor/ActionMasterTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+[TestFixture]
+public class ActionMasterTest
+{
+    private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn; // feito pra mudar o nome
+    private ActionMaster.Action tidy = ActionMaster.Action.Tidy; // feito pra mudar o nome
+    private ActionMaster actionMaster;
+
+    [SetUp] //runs every time a test runs
+    public void Setup ()
+    {
+
+        //create instante of ActionMaster
+        ActionMaster actionMaster = new ActionMaster();
+    }
+
+
+    [Test]
+    public void T00PassingTest()
+    {
+        Assert.AreEqual(1, 1);
+    }
+
+    [Test]
+    public void T01OneStrikeReturnsEndTurn ()
+    {
+        Assert.AreEqual(endTurn, actionMaster.Bowl(10));
+    }
+    
+    [Test]
+    public void T02Bowl8ReturnsTidy()
+    {
+        Assert.AreEqual(tidy, actionMaster.Bowl(8));
+    }
+
+    [Test]
+    public void T04Bowl28SpareReturnsEndTurn()
+    {
+        Assert.AreEqual(tidy , actionMaster.Bowl(8));
+        Assert.AreEqual(endTurn, actionMaster.Bowl(2));
+    }
+}
---
+++ Assets/Scripts/ActionMaster.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionMaster {
+
+    public enum Action
+    {
+        Tidy, Reset, EndTurn, EndGame //cappital as a convention
+    }
+    //private int[] bowls = new int[21]; //array starts at 0
+    private int bowl = 1;
+
+    public Action Bowl (int pins)
+    {
+        if (pins < 0 || pins > 10)
+        {
+            throw new UnityException("Invalid pins");
+        }
+
+        //other behavior herem e.g. last frame
+        if (pins == 10) //strike
+        {
+            bowl += 2;
+            return Action.EndTurn;
+            
+        }
+
+        //If first bowl of frame 
+        // return Action.Tidy;
+        if (bowl % 2 != 0) // mid frame (or last frame)
+        {
+            bowl += 1;
+            return Action.Tidy;
+        }
+
+
+
+
+        //other behavior here
+        
+        throw new UnityException("Not sure what action to return"); // throw -> exceptions
+    }
+}
